62e7c3fc2b898675e11d69e9ae9917413d5e2a4b
Ayhan Krala <codewolf27@gmail.com>
2024-01-20 17:33:41 +0300

    In the LoginGui class, added a JLabel component to display "Banking Application Login" on the form. The label is centered, styled with a bold font, and positioned appropriately.

---
+++ src/AppLauncher.java
@@ -0,0 +1,16 @@
+import guis.LoginGui;
+
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        //use invokeLater to make updates to the GUI more thread-safe
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginGui().setVisible(true);
+
+            }
+        });
+    }
+}
--- src/guis/LoginGui.java
+++ src/guis/LoginGui.java
@@ -1,7 +1,33 @@
 package guis;
+
+import javax.swing.*;
+import java.awt.*;
+
 /*
     This gui will allow user to log in or launch the register gui
     This extends from the BaseFrame, which we will need to define our own addGuiComponent()
  */
-public class LoginGui {
+public class LoginGui extends BaseFrame{
+
+    public LoginGui() {
+        super("Banking App Login");
+    }
+
+    @Override
+    protected void addGuiComponents() {
+        //create banking app label
+        JLabel bankingAppLabel = new JLabel("Banking Application");
+
+        //set the location and the size of the gui component
+        bankingAppLabel.setBounds(0,20,super.getWidth(),40);
+
+        //change the front size
+        bankingAppLabel.setFont(new Font("Dialog",Font.BOLD,32));
+
+        //center text in JLabel
+        bankingAppLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+        add(bankingAppLabel);
+    }
 }
