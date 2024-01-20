0a26f30eb1dd4a05e59aff3a3af029db66840578
Ayhan Krala <codewolf27@gmail.com>
2024-01-20 16:30:37 +0300

    Building Base Frame

--- src/guis/BaseFrame.java
+++ src/guis/BaseFrame.java
@@ -1,5 +1,37 @@
 package guis;
 
-public class BaseFrame {
-    
+import javax.swing.*;
+
+//creating an abstract class that help us set up the blueprint that our guis
+public abstract class BaseFrame extends JFrame {
+    public BaseFrame(String title){
+        initialize(title);
+    }
+
+    public void initialize(String title){
+        //instantiate frane properties and add a title to the bar
+        setTitle(title);
+
+        //set size
+        setSize(420,600);
+
+        //terminate program when qui is closed
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //set layout to null
+        setLayout(null);
+
+        //prevent qui from being resized
+
+        setResizable(false);
+
+        //launch the gui in the center
+        setLocationRelativeTo(null);
+
+        //call on the subclass
+        addGuiComponents();
+
+    }
+
+    protected abstract void addGuiComponents();
 }
