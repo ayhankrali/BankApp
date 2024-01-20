e1621d73fee68a194778a535ce625e2fa3899beb
Ayhan Krala <codewolf27@gmail.com>
2024-01-20 16:33:16 +0300

    Building Login Gui

---
+++ src/guis/LoginGui.java
@@ -0,0 +1,7 @@
+package guis;
+/*
+    This gui will allow user to log in or launch the register gui
+    This extends from the BaseFrame, which we will need to define our own addGuiComponent()
+ */
+public class LoginGui {
+}
