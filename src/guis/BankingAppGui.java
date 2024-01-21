5f2c9896ed1805fae0a729b1aac2eff3e1318195
Ayhan Krala <codewolf27@gmail.com>
2024-01-21 15:48:36 +0300

    Create and set current balance label and balance field

--- src/guis/BankingAppGui.java
+++ src/guis/BankingAppGui.java
@@ -2,18 +2,36 @@ package guis;
 
 import db_objs.User;
 
+import javax.swing.*;
+import java.awt.*;
+
 /*Performs banking such as depositing, withdrawing, seeing past transaction and transferring
 this extends from the BaseFrame
 */
 public class BankingAppGui extends BaseFrame {
     public BankingAppGui(User user) {
-        super("Banking App",user);
+        super("Banking App", user);
 
     }
 
     @Override
     protected void addGuiComponents() {
-       //create a welcome message
+        //create a welcome message
+        String welcomeMessage = "<html>" +
+                "<body style='text-align:center'>" +
+                "<b>Hello " + user.getUsername() + "</b><br>" +
+                "What would you like to do today?</body></html>";
+        JLabel welcomeMessageLabel = new JLabel(welcomeMessage);
+        welcomeMessageLabel.setBounds(0, 20, getWidth() - 10, 40);
+        welcomeMessageLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
+        welcomeMessageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(welcomeMessageLabel);
 
+        //create current balance label
+        JLabel currentBalanceLabel = new JLabel("Current Balance");
+        currentBalanceLabel.setBounds(0, 80, getWidth() - 10, 30);
+        currentBalanceLabel.setFont(new Font("Dialog", Font.BOLD, 22));
+        currentBalanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(currentBalanceLabel);
     }
 }
