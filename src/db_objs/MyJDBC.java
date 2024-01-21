357759492f4f277ca1b59e84a41075c70c8f3513
Ayhan Krala <codewolf27@gmail.com>
2024-01-21 19:56:17 +0300

    Implemented user registration functionality in RegisterGui with input validation and database interaction

--- src/db_objs/MyJDBC.java
+++ src/db_objs/MyJDBC.java
@@ -13,8 +13,8 @@ public class MyJDBC {
 
 
     // if valid, return an object with the user's information
-    public static User validateLogin(String username, String password){
-        try{
+    public static User validateLogin(String username, String password) {
+        try {
             // establish a connection to the database using configurations
             Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
@@ -34,7 +34,7 @@ public class MyJDBC {
             // next() returns true or false
             // true - query returned data and result set now points to the first row
             // false - query returned no data, and a result set equals to null
-            if(resultSet.next()){
+            if (resultSet.next()) {
                 // success
                 // get id
                 int userId = resultSet.getInt("id");
@@ -46,11 +46,70 @@ public class MyJDBC {
                 return new User(userId, username, password, currentBalance);
             }
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
         // not valid user
         return null;
     }
+
+    public static boolean register(String username, String password) {
+        try {
+            //need first to check if the username has already been taken
+            // first we will need to check if the username has already been taken
+            if (!checkUser(username)) {
+                Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO users(username, password, current_balance) " +
+                                "VALUES(?, ?, ?)"
+                );
+
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+                preparedStatement.setBigDecimal(3, new BigDecimal(0));
+
+                preparedStatement.executeUpdate();
+                return true;
+
+            }
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    //check if username
+    //true - user exists
+    //false - user doesn't exist
+
+    private static boolean checkUser(String username) {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM users WHERE username = ?"
+            );
+
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // this means that the query returned no data meaning that the username is available
+            if (!resultSet.next()) {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+
+
+    }
 }
+
+
+
+
--- src/guis/RegisterGui.java
+++ src/guis/RegisterGui.java
@@ -1,7 +1,13 @@
 package guis;
 
+import db_objs.MyJDBC;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class RegisterGui extends BaseFrame {
     public RegisterGui() {
@@ -64,10 +70,48 @@ public class RegisterGui extends BaseFrame {
         rePasswordField.setFont(new Font("Dialog", Font.PLAIN, 28));
         add(rePasswordField);
 
-        //create login button
+        //create register button
         JButton registerButton = new JButton("Register");
         registerButton.setBounds(20, 460, getWidth() - 50, 40);
         registerButton.setFont(new Font("Dialog", Font.BOLD, 23));
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //get username
+                String username = usernameField.getText();
+
+                // get password
+                String password = String.valueOf(passwordField.getPassword());
+
+                // get re password
+                String rePassword = String.valueOf(rePasswordField.getPassword());
+
+                //we will need to validate the user input
+                if(validateUserInput(username, password, rePassword)){
+                    // attempt to register the user to the database
+                    if(MyJDBC.register(username, password)){
+                        // register success
+                        // dispose of this gui
+                        RegisterGui.this.dispose();
+
+                        // launch the login gui
+                        LoginGui loginGui = new LoginGui();
+                        loginGui.setVisible(true);
+
+                        // create a result dialog
+                        JOptionPane.showMessageDialog(loginGui, "Registered Account Successfully!");
+                    }else{
+                        // register failed
+                        JOptionPane.showMessageDialog(RegisterGui.this, "Error: Username already taken");
+                    }
+                }else{
+                    // invalid user input
+                    JOptionPane.showMessageDialog(RegisterGui.this,
+                            "Error: Username must be at least 6 characters\n" +
+                                    "and/or Password must match");
+                }
+            }
+        });
         add(registerButton);
 
         // create login label
@@ -75,9 +119,31 @@ public class RegisterGui extends BaseFrame {
         loginLabel.setBounds(0, 510, getWidth() - 10, 30);
         loginLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
         loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        loginLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //dispose of this gui
+                RegisterGui.this.dispose();
+
+                //launch the login gui
+                new LoginGui().setVisible(true);
+            }
+        });
         add(loginLabel);
 
-
-
     }
+    private boolean validateUserInput(String username, String password, String rePassword){
+        // all fields must have a value
+        if(username.length() == 0 || password.length() == 0 || rePassword.length() == 0) return false;
+
+        // the username has to be at least 6 characters long
+        if(username.length() < 6) return false;
+
+        // password and repassword must be the same
+        if(!password.equals(rePassword)) return false;
+
+        // passes validation
+        return true;
+    }
+
 }
