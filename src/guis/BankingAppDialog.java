bc511f6fd809fa51f4c73409641476f5ce0adc93
Ayhan Krala <codewolf27@gmail.com>
2024-01-22 14:38:08 +0300

    Add functionality to the deposit, withdraw and Transfer buttons

--- src/guis/BankingAppDialog.java
+++ src/guis/BankingAppDialog.java
@@ -3,6 +3,97 @@ package guis;
   Display a custom for BankingAppGui
 */
 
-public class BankingAppDialog {
+import db_objs.User;
 
+import javax.swing.*;
+import java.awt.*;
+
+
+public class BankingAppDialog extends JDialog {
+    private User user;
+    private BankingAppGui bankingAppGui;
+    private JLabel balanceLabel, enterAmountLabel, enterUserLabel;
+    private JTextField enterAmountField, enterUserField;
+    private JButton actionButton;
+    //private JPanel pastTransactionPanel;
+    // private ArrayList<Transaction> pastTransactions;
+
+    public BankingAppDialog(BankingAppGui bankingAppGui, User user) {
+        // set the size
+        setSize(400, 400);
+
+        // add focus to the dialog (can't interact with anything else until dialog is closed)
+        setModal(true);
+
+        // loads in the center of our banking gui
+        setLocationRelativeTo(bankingAppGui);
+
+        // when suer closes the dialog, it releases its resources that are being used
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        // prevents dialog from being resized
+        setResizable(false);
+
+        // allows us to manually specify the size and position of each component
+        setLayout(null);
+
+        // we will need reference to our gui so that we can update the current balance
+        this.bankingAppGui = bankingAppGui;
+
+        // we will need access to the user info to make updates to our db or retrieve data about the user
+        this.user = user;
+
+
+    }
+
+
+    public void addCurrentBalanceAndAmount() {
+        // balance label
+        balanceLabel = new JLabel("Balance: $" + user.getCurrentBalance());
+        balanceLabel.setBounds(0, 10, getWidth() - 20, 20);
+        balanceLabel.setFont(new Font("Dialog", Font.BOLD, 16));
+        balanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(balanceLabel);
+
+        // enter amount label
+        enterAmountLabel = new JLabel("Enter Amount:");
+        enterAmountLabel.setBounds(0, 50, getWidth() - 20, 20);
+        enterAmountLabel.setFont(new Font("Dialog", Font.BOLD, 16));
+        enterAmountLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(enterAmountLabel);
+
+        // enter amount field
+        enterAmountField = new JTextField();
+        enterAmountField.setBounds(15, 80, getWidth() - 50, 40);
+        enterAmountField.setFont(new Font("Dialog", Font.BOLD, 20));
+        enterAmountField.setHorizontalAlignment(SwingConstants.CENTER);
+        add(enterAmountField);
+    }
+
+
+    public void addActionButton(String actionButtonType) {
+        actionButton = new JButton(actionButtonType);
+        actionButton.setBounds(15, 300, getWidth() - 50, 40);
+        actionButton.setFont(new Font("Dialog", Font.BOLD, 20));
+        actionButton.setHorizontalAlignment(SwingConstants.RIGHT);
+        add(actionButton);
+
+    }
+
+    public void addUserField(){
+        //enter user label
+        enterUserLabel = new JLabel("Enter User:");
+        enterUserLabel.setBounds(0, 160, getWidth() - 20, 20);
+        enterUserLabel.setFont(new Font("Dialog", Font.BOLD, 16));
+        enterUserLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(enterUserLabel);
+
+        //enter user field
+        enterUserField = new JTextField();
+        enterUserField.setBounds(15, 190, getWidth() - 50, 40);
+        enterUserField.setFont(new Font("Dialog", Font.BOLD, 20));
+        enterUserField.setHorizontalAlignment(SwingConstants.CENTER);
+        add(enterUserField);
+
+    }
 }
--- src/guis/BankingAppGui.java
+++ src/guis/BankingAppGui.java
@@ -4,13 +4,18 @@ import db_objs.User;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /*Performs banking such as depositing, withdrawing, seeing past transaction and transferring
 this extends from the BaseFrame
 */
-public class BankingAppGui extends BaseFrame {
+public class BankingAppGui extends BaseFrame implements ActionListener {
     private JTextField currentBalanceField;
-    public JTextField getCurrentBalancedField(){return currentBalanceField;}
+
+    public JTextField getCurrentBalancedField() {
+        return currentBalanceField;
+    }
 
 
     public BankingAppGui(User user) {
@@ -51,32 +56,87 @@ public class BankingAppGui extends BaseFrame {
         JButton depositButton = new JButton("Deposit");
         depositButton.setBounds(15, 180, getWidth() - 50, 50);
         depositButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        depositButton.addActionListener(this);
         add(depositButton);
 
         //withdraw button
         JButton withdrawButton = new JButton("Withdraw");
         withdrawButton.setBounds(15, 250, getWidth() - 50, 50);
         withdrawButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        withdrawButton.addActionListener(this);
         add(withdrawButton);
 
         //past transaction button
         JButton pastTransactionButton = new JButton("Past Transaction");
         pastTransactionButton.setBounds(15, 320, getWidth() - 50, 50);
         pastTransactionButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        pastTransactionButton.addActionListener(this);
         add(pastTransactionButton);
 
         //transfer button
         JButton transferButton = new JButton("Transfer");
         transferButton.setBounds(15, 390, getWidth() - 50, 50);
         transferButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        transferButton.addActionListener(this);
         add(transferButton);
 
         //logout button
         JButton logoutButton = new JButton("Logout");
         logoutButton.setBounds(15, 500, getWidth() - 50, 50);
         logoutButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        logoutButton.addActionListener(this);
         add(logoutButton);
 
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String buttonPressed = e.getActionCommand();
+
+        //user pressed logout
+        if (buttonPressed.equalsIgnoreCase("Logout")) {
+
+            //return user to the login gui
+            new LoginGui().setVisible(true);
+
+            //dispose of this gui
+            this.dispose();
+
+            //don't bother running the rest of the code
+            return;
+
+        }
+        //other functions
+        BankingAppDialog bankingAppDialog = new BankingAppDialog(this, user);
+
+        //set the title of the dialog header to the action
+        bankingAppDialog.setTitle(buttonPressed);
+
+        // if the button pressed is deposit, withdraw, or transfer
+        if (buttonPressed.equalsIgnoreCase("Deposit") || buttonPressed.equalsIgnoreCase("Withdraw")
+                || buttonPressed.equalsIgnoreCase("Transfer")) {
+            // add in the current balance and amount gui components to the dialog
+            bankingAppDialog.addCurrentBalanceAndAmount();
+
+
+            // add action button
+            bankingAppDialog.addActionButton(buttonPressed);
+
+
+            // for the transfer action it will require more components
+            if (buttonPressed.equalsIgnoreCase("Transfer")) {
+                bankingAppDialog.addUserField();
+            }
+
+        /*}else if(buttonPressed.equalsIgnoreCase("Past Transaction")){
+            bankingAppDialog.addPastTransactionComponents();
+        }*/
+
+        // make the app dialog visible
+        bankingAppDialog.setVisible(true);
+
+
+        }
     }
 }
